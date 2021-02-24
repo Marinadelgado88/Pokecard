1049eaf0126ee96c8959ff56b494ed70e084c035
Marina Delgado <marinadelges@gmail.com>
2021-02-24 08:49:49 +0100

    add innitial commit

---
+++ src/components/App.js
@@ -0,0 +1,8 @@
+import PokeList from './PokeList';
+import data from '../data/data.json';
+
+function App() {
+  <ClubList clubs={clubs} />;
+}
+
+export default App;
---
+++ src/components/PokeList.js
@@ -0,0 +1,11 @@
+import Pokemon from './Pokemon';
+
+const Pokelist = (props) => {
+  return (
+    <div>
+      <li></li>
+    </div>
+  );
+};
+
+export default Pokelist;
